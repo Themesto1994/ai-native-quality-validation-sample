736eb70cefc6fc310869f221010b877cf1635605
Themesto1994 <123085380+Themesto1994@users.noreply.github.com>
2026-08-16 23:30:23 +0530

    Ghaziabad removal.tsx

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -22,7 +22,6 @@ const LOCATIONS = [
   { city: 'New Orleans', state: 'Louisiana', country: 'United States' },
   { city: 'Denver', state: 'Colorado', country: 'United States' },
   { city: 'Seoul', state: 'South Korea', country: 'South Korea' },
-  { city: 'Ghaziabad', state: 'Uttar Pradesh', country: 'India' },
 ];
 
 const SearchBar = ({ onSearch, initialFilters }: SearchBarProps) => {
